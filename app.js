ca0154bf32f3ac142557157a3c451e6f46c4d7d0
Daniel Bennington <dbennington125@gmail.com>
2020-03-31 17:57:26 -0700

    display results working

--- app.js
+++ app.js
@@ -4,29 +4,51 @@ const myButton = document.getElementById('button'); //getting the button element
 const userInput = document.getElementById('input'); //getting the input element
 const userGuessSpan = document.getElementById('numberGuess');//the number the user guessed
 const triesLeftOutput = document.getElementById('remaingTries');//the remaining tries left
-
-//const numberTest =10;
+const answer = document.getElementById('correctNumber');
+//get
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+
+
+
 let actualNumber = getRandomInt(1, 20);
 console.log(actualNumber);
 
+//making a function to decrease 4-0
+
+//let countdown = 4; 
+//countdown--;
+//countdown.textContent = triesLeftOutput;
+    
+    
 function playGame() {
-    const actualUserInput = userInput.value;
-    //console.log(actualUserInput);
-    const comparedNumbers = compareNumbers(actualUserInput, actualNumber);
-    console.log(comparedNumbers);
+    const actualUserInput = Number(userInput.value);
+        //console.log(actualUserInput); works
+    const result = compareNumbers(actualUserInput, actualNumber);
+        //console.log(comparedNumbers); works
+        
+        
+    function displayResult(result) {
+        if (result === 0) {
+            answer.textContent = 'Great Job!';
+        } else if (result === 1) {
+            answer.textContent = 'Nope, too high';
+        } else if (result === -1) {
+            answer.textContent = 'Nope, too low';
+        }}
+            
+    displayResult(result);
+}
+        
+        //function youGotIt() {
+            //answer.textContent = 'Great Job!';
+            //}
+//function noDice() {
+    //answer.textContent = 'Nope, try again';
+//}
 
-
- }
-myButton.addEventListener('click',playGame);
-//Gus = userInput.value
-//let = 5;
-//creat function which has win/loss/out of tries
-
-
-//
-//console.log(triesLeftOutput); //giving the tries left back to the element.
+myButton.addEventListener('click', playGame);
