ab1caf9451f8b036e26173292571addfee4cae9d
Daniel Bennington <dbennington125@gmail.com>
2020-03-31 19:47:19 -0700

    try

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 import compareNumbers from './compareNumber.js';//import compare function from other js script export 
 
 const myButton = document.getElementById('button'); //getting the button element
+const guessOut = document.getElementById('numberGuess');//the number the user guessed
 const userInput = document.getElementById('input'); //getting the input element
-const userGuessSpan = document.getElementById('numberGuess');//the number the user guessed
-const triesLeftOutput = document.getElementById('remaingTries');//the remaining tries left
+
+function goNow() {
+    const userInput = document.getElementById('input');
+    const guessOutput = userInput.value;
+    const guessOut = document.getElementById('numberGuess');
+    guessOut.textContent = guessOutput;
+    //console.log(guessOutput);
+}
+myButton.addEventListener('click', goNow);
+
+function trybutton() {
+    let countdown = 4;
+    const clickDecrease = countdown--;
+    const tries = document.getElementById('remaingTries');
+    tries.textContent = clickDecrease;
+    console.log(trybutton);
+}
+myButton.addEventListener('click', trybutton);
+
+//const triesLeftOutput = document.getElementById('remaingTries');//the remaining tries left
 const answer = document.getElementById('correctNumber');
-//get
+
+//math function found to work
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min)) + min; 
 }
 
-
-
-
 let actualNumber = getRandomInt(1, 20);
-console.log(actualNumber);
+//console.log(actualNumber);
+
 
 //making a function to decrease 4-0
 
@@ -43,12 +61,6 @@ function playGame() {
             
     displayResult(result);
 }
-        
-        //function youGotIt() {
-            //answer.textContent = 'Great Job!';
-            //}
-//function noDice() {
-    //answer.textContent = 'Nope, try again';
-//}
+              
 
 myButton.addEventListener('click', playGame);
